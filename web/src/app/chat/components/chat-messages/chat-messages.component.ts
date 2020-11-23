f7b02d373b562f903eee4931bebc2f377431b47b
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-11-23 09:53:28 -0600

    Behavior fixes

--- web/src/app/chat/components/chat-messages/chat-messages.component.ts
+++ web/src/app/chat/components/chat-messages/chat-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { SocketIoService } from "../../../socket/socket-io.service";
 import { SocketMessage } from "../../socket-message";
 import { Store } from "@ngrx/store";
@@ -6,6 +6,7 @@ import { AppState } from "../../../app.store";
 import { appendMessage } from "../../chat.actions";
 import { Observable } from "rxjs";
 import { Chat } from "../../chat";
+import { User } from "../../../user/user";
 
 @Component({
   selector: 'app-chat-messages',
@@ -14,14 +15,22 @@ import { Chat } from "../../chat";
 })
 export class ChatMessagesComponent implements OnInit {
 
+  $user!: Observable<User | null>;
   $chat!: Observable<Chat | null>;
-  constructor(private socket: SocketIoService, private store: Store<AppState>) {
+  constructor(
+    public element: ElementRef,
+    private socket: SocketIoService,
+    private store: Store<AppState>
+  ) {
+    console.log(element.nativeElement);
+    this.$user = this.store.select(state => state.user);
     this.$chat = this.store.select(state => state.chat.current);
   }
 
   ngOnInit(): void {
     console.log("listen messages from server")
     this.socket.io.on("chat.message", this.handleIncomingMessage.bind(this));
+    this.socket.io.on('connect', this.scrollToBottom.bind(this));
   }
 
   /**
@@ -31,6 +40,15 @@ export class ChatMessagesComponent implements OnInit {
   handleIncomingMessage(payload: SocketMessage<any>) {
     const { room, message } = payload;
     // update chat messages
-    this.store.dispatch(appendMessage({ chatId: room, message }))
+    this.store.dispatch(appendMessage({ chatId: room, message }));
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    this.element.nativeElement.scroll({
+      top: this.element.nativeElement.scrollHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 }
